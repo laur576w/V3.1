026fb02fd79fe4f8756615811e4790b42ebfe167
laur576w <114057522+laur576w@users.noreply.github.com>
2023-05-25 10:37:21 +0200

    opgave 5

--- php/Opgave 3/index.php
+++ php/Opgave 3/index.php
@@ -27,7 +27,96 @@
         }
         return "word '$word' is not a palindrom";
     }
-    
+
+    //------------
+    // opgave 5 --
+    //------------
+
+    // opgave 5.2
+    $students = ["nikolai","rasmus", "marius", "nikolaj", "jonas", "laurits"];
+    $find = "rasmus";
+
+    function myFindIndexInArray(
+        $find, 
+        $array) {
+        //checks if what it should search is found
+        if(!in_array($find, $array)){
+            return "'" . ucfirst($find) . "' is not found in the array";
+        }
+
+        $result = "'" . ucfirst($find) . "' is found at index: ";
+        foreach( $array as $index => $item ) {
+            if (strtolower($item) == strtolower($find)) {
+                $result .=  "$index ";
+            }
+        }
+        return $result;
+    }
+
+    // opgave 5.3
+    function findIndexInArray(
+        $find, 
+        $array) {
+            //checks if what it should search is found
+            if(!in_array($find, $array)){
+                return "'" . ucfirst($find) . "' is not found in the array";
+            }
+            return "'" . ucfirst($find) . "' is found in index: " . array_search($find, $array, false) ;
+    }
+
+    //opgave 5.4
+    $months = [
+        "januar" => 31,
+        "marts" => 31,
+        "maj" => 31,
+        "juli" => 31,
+        "august" => 31,
+        "oktober" => 31,
+        "december" => 31,
+        "februar" => 28,
+        "april" => 30,
+        "juni" => 30,
+        "september" => 30,
+        "november" => 30
+    ];
+
+
+    $i = 1;
+    foreach($months as $month => $days) {
+        switch(true) {
+            case ($days <= 30):
+                $smonth[$month] = $days;
+                break;    
+            case ($days >= 31):
+                $lmonth[$month] = $days;
+                break;
+        }
+    }
+
+    // Opgave 5.5
+    $laerere = array(
+        array(
+        "fornavn" => "Hanne",
+        "efternavn" => "Lund",
+        "fag" => "Visualisering"
+        ),
+        array(
+        "fornavn" => "Jens",
+        "efternavn" => "Clausen",
+        "fag" => "Softwarekonstruktion"
+        ),
+        array(
+        "fornavn" => "Ronni",
+        "efternavn" => "Hansen",
+        "fag" => "Teknik"
+        ),
+        array(
+        "fornavn" => "Ulf",
+        "efternavn" => "Skaaning",
+        "fag" => "AspIT-Lab"
+        )
+    );
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,33 +128,71 @@
     <title>Opgave 3</title>
 </head>
 <body>
-    
-    <p><?= strtoupper($string); ?></p>
-    <p><?= ucfirst($string); ?></p>
-    <p><?= ucwords($string); ?></p>
-    <p>Sentence contains: <?= str_contains($string, "fox") ?> fox</p>
-    <p><?= strstr($email, "@", true); ?></p>
-    <!-- <iframe src="password.php" frameborder="1"></iframe> -->
-    <form method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <label for="length">Length of password</label>
-        <input type="number" name="length">
-        <button type="submit">Generate</button>
-        <?php if ($_POST["length"]) {?>
-        <p>
-            Password: <br>
-            <code style="word-wrap: break-word;"><?= passwordGen($_POST["length"])?></code>
-        </p>
+    <div class="container">
+
+        <p><?= strtoupper($string); ?></p>
+        <p><?= ucfirst($string); ?></p>
+        <p><?= ucwords($string); ?></p>
+        <p>Sentence contains: <?= str_contains($string, "fox") ?> fox</p>
+        <p><?= strstr($email, "@", true); ?></p>
+        <!-- <iframe src="password.php" frameborder="1"></iframe> -->
+        <form method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label for="length">Length of password</label>
+            <input type="number" name="length">
+            <button type="submit">Generate</button>
+            <?php if ($_POST["length"]) {?>
+            <p>
+                Password: <br>
+                <code style="word-wrap: break-word;"><?= passwordGen($_POST["length"])?></code>
+            </p>
+            
+            <?php }?> 
+        </form>
         
-        <?php }?> 
-    </form>
+        <form method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label for="word">Is the word a palindrom?</label>
+            <input type="text" name="word">
+            <button type="submit">Tjek</button>
+            <?php if ($_POST["word"]) { ?>
+            <p><?= palindrom($_POST["word"]) ?></p>
+            <?php } ?>
+        </form>
     
-    <form method="post" action="<?= htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <label for="word">Is the word a palindrom?</label>
-        <input type="text" name="word">
-        <button type="submit">Tjek</button>
-        <?php if ($_POST["word"]) { ?>
-        <p><?= palindrom($_POST["word"]) ?></p>
-        <?php } ?>
-    </form>
+        <hr>
+    
+        <p>Does '<?= ucfirst($find) ?>' exist in the array. <?= myFindIndexInArray($find, $students) ?></p>
+        <p>Does '<?= ucfirst($find) ?>' exist in the array. <?= findIndexInArray($find, $students) ?></p>
+    
+        <hr>
+        <div class="cols-2">
+            <div class="col">  
+                <h3>Short months</h3>
+                <pre ><?php 
+                        foreach($smonth as $month => $day) {
+                            echo "$month: $days days <br>";
+                        } 
+                    ?></pre>
+            </div>
+                
+            <div class="col">
+                <h3>Long months</h2>
+                <pre><?php foreach($lmonth as $month => $day) {
+                            echo "$month: $days days <br>";
+                        } 
+                ?></pre>
+            </div>
+        </div>  
+        <hr>
+        <pre><?php print_r($laerere) ?></pre>  
+        <pre><?php 
+            foreach($laerere as $person) {
+                foreach($person as $index => $val){
+                    echo ucfirst($index) . ": $val <br>";
+                }
+                echo "<br>";
+            }
+        ?></pre>
+    </div>
+
 </body>
 </html>
